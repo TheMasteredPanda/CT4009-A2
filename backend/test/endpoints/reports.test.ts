11d9842b9b4cacf6c7df872261617ec73591fb90
Duke J. Morgan <dukejakemorgan@gmail.com>
2020-05-05 20:17:48 +0100

    Finished writing basic test units for all reports endpoints.

--- backend/test/endpoints/reports.test.ts
+++ backend/test/endpoints/reports.test.ts
@@ -4,6 +4,7 @@ import * as databaseManager from "../../src/managers/databaseManager";
 import supertest from "supertest";
 import * as authManager from "../../src/managers/authManager";
 import { ModelCtor, Model } from "sequelize/types";
+import { endianness } from "os";
 let request = supertest("http://localhost:4000");
 
 beforeAll(async (done) => {
@@ -61,4 +62,205 @@ afterAll(async (done) => {
   await authManager.flushAll();
   await shutdown();
   done();
+});
+
+function createJohnDoe() {
+  return new Promise((resolve, reject) => {
+    request
+      .post("/user/register")
+      .send({
+        username: "johndoe",
+        password: "password1",
+        email: "johndoe@gmail.com",
+      })
+      .end((err, res) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        expect(res.status).toBe(200);
+        resolve(res.body);
+      });
+  });
+}
+
+function createBikeEntry(authPayload: any) {
+  return new Promise((resolve, reject) => {
+    request
+      .post(`/bikes/register?bikeId=${authPayload.id}`)
+      .send({ brand: "Test Brand", wheel_size: 24.3, gear_count: 8 })
+      .end((err, res) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        expect(res.status).toBe(200);
+        resolve(res.body.id);
+      });
+  });
+}
+
+describe("Testing Report Endpoints", () => {
+  it("/reports/create", (done) => {
+    createJohnDoe().then((authPayload: any) => {
+      createBikeEntry(authPayload).then((bikeId: any) => {
+        request
+          .post(`/reports/create?userId=${authPayload.id}&bikeId=${bikeId}`)
+          .set({ content: "Test Report" })
+          .set("Authorization", `Bearer ${authPayload.token}`)
+          .end((err, res) => {
+            if (err) throw err;
+            expect(res.status).toBe(200);
+            expect(res.body).toBeDefined();
+            done();
+          });
+      });
+    });
+  });
+
+  it("/reports", (done) => {
+    createJohnDoe().then((authPayload: any) => {
+      createBikeEntry(authPayload).then((bikeId: any) => {
+        request
+          .post(`/reports/create?userId=${authPayload.id}&bikeId=${bikeId}`)
+          .send({ content: "Test Report" })
+          .set("Authorization", `Bearer ${authPayload.token}`)
+          .end((err, res) => {
+            if (err) throw err;
+            expect(res.status).toBe(200);
+            expect(res.body).toBeDefined();
+            request
+              .get(`/reports?userId=${authPayload.id}`)
+              .set("Authorization", `Bearer ${authPayload.token}`)
+              .end((err, res) => {
+                if (err) throw err;
+                expect(res.status).toBe(200);
+                expect(res.body).toBeDefined();
+                done();
+              });
+          });
+      });
+    });
+  });
+
+  it("/reports/report", (done) => {
+    createJohnDoe().then((authPayload: any) => {
+      createBikeEntry(authPayload).then((bikeId: any) => {
+        request
+          .post(`/reports/create?userId=${authPayload.id}&bikeId=${bikeId}`)
+          .set("Authorization", `Bearer ${authPayload.token}`)
+          .send({ content: "Test Report" })
+          .end((err, res) => {
+            if (err) throw err;
+            expect(res.status).toBe(200);
+            expect(res.body).toBeDefined();
+            request
+              .get(
+                `/reports/report?userId=${authPayload.id}&reportId=${res.body.id}`
+              )
+              .set("Authorizatin", `Bearer ${authPayload.token}`)
+              .end((err, res) => {
+                if (err) throw err;
+                expect(res.status).toBe(200);
+                expect(res.body).toBeDefined();
+                done();
+              });
+          });
+      });
+    });
+  });
+
+  it("/reports/comments/create", (done) => {
+    createJohnDoe().then((authPayload: any) => {
+      createBikeEntry(authPayload).then((bikeId: any) => {
+        request
+          .post(`/reports/create?userId=${authPayload.id}&bikeId=${bikeId}`)
+          .send({ content: "Test Report" })
+          .set("Authorization", `Bearer ${authPayload.token}`)
+          .end((err, res) => {
+            if (err) throw err;
+            expect(res.status).toBe(200);
+            expect(res.body).toBeDefined();
+            request
+              .post(
+                `/reports/comments/create?userId=${authPayload.id}&bikeId=${bikeId}`
+              )
+              .set("Authorization", `Bearer ${authPayload.token}`)
+              .send({ comment: "This is a test comment", type: "civilian" })
+              .end((err, res) => {
+                if (err) throw err;
+                expect(res.status).toBe(200);
+                expect(res.body).toBeDefined();
+                done();
+              });
+          });
+      });
+    });
+  });
+
+  it("/reports/comments", (done) => {
+    createJohnDoe().then((authPayload: any) => {
+      createBikeEntry(authPayload).then((bikeId: any) => {
+        request
+          .post(`/reports/create?userId=${authPayload.id}&bikeId=${bikeId}`)
+          .send({ content: "Test Reports" })
+          .set("Authorization", `Bearer ${authPayload.token}`)
+          .end((err, res) => {
+            if (err) throw err;
+            expect(res.status).toBe(200);
+            expect(res.body).toBeDefined();
+            let reportId = res.body.id;
+            request
+              .post(
+                `/reports/comments/create?userId=${authPayload.id}&bikeId=${bikeId}`
+              )
+              .send({ comment: "This is a test comment.", type: "civilian" })
+              .set("Authorization", `Bearer ${authPayload.token}`)
+              .end((err, res) => {
+                if (err) throw err;
+                expect(res.status).toBe(200);
+                expect(res.body).toBeDefined();
+                request
+                  .get(
+                    `/reports/comments?userId=${authPayload.id}&reportId=${reportId}&type=civilian`
+                  )
+                  .set("Authorization", `Bearer ${authPayload.token}`)
+                  .end((err, res) => {
+                    if (err) throw err;
+                    expect(res.status).toBe(200);
+                    expect(res.body).toBeDefined();
+                    done();
+                  });
+              });
+          });
+      });
+    });
+  });
+
+  it("/reports/close", (done) => {
+    createJohnDoe().then((authPayload: any) => {
+      createBikeEntry(authPayload).then((bikeId: any) => {
+        request
+          .post(`/reports/create?userId=${authPayload.id}&bikeId=${bikeId}`)
+          .send({ content: "Test Content" })
+          .set("Authorization", `Bearer ${authPayload.token}`)
+          .end((err, res) => {
+            if (err) throw err;
+            expect(res.status).toBe(200);
+            expect(res.body).toBeDefined();
+            request
+              .post(`/reports/close?userId=${authPayload.id}&bikeId=${bikeId}`)
+              .set("Authorization", `Bearer ${authPayload.token}`)
+              .end((err, res) => {
+                if (err) throw err;
+                expect(res.status).toBe(200);
+                expect(res.body).toBeDefined();
+                done();
+              });
+          });
+      });
+    });
+  });
 });
