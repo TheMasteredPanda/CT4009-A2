8ae6b974ba4b1d6f174195e1002b23b348df4357
Duke J. Morgan <dukejakemorgan@gmail.com>
2020-05-17 18:13:36 +0100

    Finished admin endpoints documentation.

--- backend/src/endpoints/admin.ts
+++ backend/src/endpoints/admin.ts
@@ -5,6 +5,9 @@ import * as adminActions from "../actions/admin";
 
 const router = Router();
 
+/**
+ * Middleware used to ensure that only admins can access these functions.
+ */
 router.use((req: Request, res: Response, next: Function) => {
   if (!req.originalUrl.startsWith("/admin")) {
     next();
@@ -22,6 +25,29 @@ router.use((req: Request, res: Response, next: Function) => {
   next();
 });
 
+/**
+ * @api {post} /admin/accounts/create   Create an officer account.
+ * @apiVersion 0.1.0
+ * @apiDescription                      Creates a new officer account.
+ * @apiGroup endpoints/admin
+ *
+ * @apiParam (Body Param) username      The name for this account.
+ * @apiParam (Body Param) password      The password for this account.
+ * @apiParam (Body Param) email         The email address for this account.
+ *
+ * @apiParamExample Example:
+ *      {
+ *        "username": "johndoe",
+ *        "password": "password1",
+ *        "email": "johndoe@gmail.com"
+ *      }
+ *
+ * @apiError {BadRequest} 400           Either the username, password, or email parameter is missing.
+ * @apiError {NotAcceptable} 406        The username is taken or the email is already in use.
+ *
+ * @apiSuccess {Sucess} {string} id     The id of the newly created officer account.
+ *
+ */
 router.post("/admin/accounts/create", (req: Request, res: Response) => {
   let body = req.body;
 
@@ -63,6 +89,24 @@ router.post("/admin/accounts/create", (req: Request, res: Response) => {
     .catch((err) => handleInternalError(res, err));
 });
 
+/**
+ * @api {post} /admin/accounts      Fetch all user accounts, or specified accounts.
+ * @apiVersion 0.1.0
+ * @apiGroup endpoints/admin
+ * @apiDescription                  Fetches all users account information, save for their
+ *                                  passwords. If an array of ids are specified in the body
+ *                                  of this request, then it will fetch all accounts in that
+ *                                  array.
+ *
+ * @apiParam (Body Param) accounts - An array of account ids. Optional.
+ *
+ * @apiParamExample {json} Example:
+ *      {
+ *        "accounts": [1,2,3,4,5]
+ *      }
+ *
+ * @apiSuccess {Success} {object[]} An array of accounts.
+ */
 router.post("/admin/accounts", (req: Request, res: Response) => {
   let accounts = [];
 
@@ -76,6 +120,24 @@ router.post("/admin/accounts", (req: Request, res: Response) => {
     .catch((err) => handleInternalError(res, err));
 });
 
+/**
+ * @api {post} /admin/accounts/account      Get account details
+ * @apiVersion 0.1.0
+ * @apiGroup endpoints/admin
+ *
+ * @apiDescription                          Fetches information on the account id given
+ *                                          In the request. This includes images and
+ *                                          contacts
+ *
+ * @apiParam (Query Param) accountId         The id of the account to fetch information abount.
+ *
+ * @apiError {BadRequest} 400                The accountId query paramter was not found.
+ * @apiError {NotFound} 404                  The accountId does not correlate to a user
+ *                                           stored in the database
+ *
+ * @apiSuccess {Success} {object}            The account object.
+ *
+ */
 router.get("/admin/accounts/account", (req: Request, res: Response) => {
   let query = req.query;
 
@@ -94,6 +156,18 @@ router.get("/admin/accounts/account", (req: Request, res: Response) => {
     .catch((err) => handleInternalError(res, err));
 });
 
+/**
+ * @api {post} /admin/accounts/delete     Delete an account
+ * @apiVersion 0.1.0
+ * @apiGroup endpoints/admin
+ * @apiDescription                        Deletes an account from the database.
+ *
+ * @apiParam (Body Param) accountId  The id of the account to delete.
+ *
+ * @apiError {BadRequest} 400             The accountId query parameter was not found.
+ *
+ * @apiSuccess {Success} {void}           A 200 status code.
+ **/
 router.post("/admin/accounts/delete", (req: Request, res: Response) => {
   let query = req.query;
 
